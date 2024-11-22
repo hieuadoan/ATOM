efa10057abffe857b31238f86fc62a47e662fd4c
Hieu Doan <hieu.a.doan@gmail.com>
2024-11-22 15:59:22 -0600

    Updated texts

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -4,7 +4,7 @@ import ProjectList from '../components/ProjectList';
 function Dashboard() {
     return (
         <div className="dashboard">
-            <h2>Welcome to the ATOM Dashboard</h2>
+            {/*<h2>Welcome to the ATOM Dashboard</h2>*/}
             <ProjectList />
         </div>
     );
